d661cd7c55501255ec9eecadd0af59d0865dfffa
Denisse Dominguez <A01702603@itesm.mx>
2022-03-17 12:30:17 -0600

    Capas unicas de DynamicTech

--- app.js
+++ app.js
@@ -2,18 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const app = express();
+// evitar ataques
 const csrf = require('csurf');
 const csrfProtection = csrf();
 
-
-const rutas_audiovisual = require('./routes/audiovisual.routes');
-const rutas_musica = require('./routes/musica.routes');
+// pasamos a las rutas
 const rutas_users = require('./routes/user.routes');
 const path = require('path');
-
-
-
-const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -23,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(session({
-    secret: 'Hola soy Jordana', 
+    secret: 'Hola Zebrands', 
     resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
     saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
 }));
@@ -38,22 +34,11 @@ app.use(session({
 
 //Para login
 app.use('/users', rutas_users);
-//Para audio visuales
-app.use('/audioVisual', rutas_audiovisual);
-//Para música
-app.use('/musica', rutas_musica);
-
 
 //Middleware página principal
 app.use((request, response, next) => {
     response.redirect('/users');
     next();
-});
-
-
-app.use((request, response, next) => {
-    console.log('Otro middleware!');
-    response.send('Aquí no hay nada'); //Manda la respuesta
 });
 
 app.listen(3000);
