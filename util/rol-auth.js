ebee3ca3b78743ffd55f52f507b635f763fdca11
Karen Baltazar <a01707750@tec.mx>
2022-03-16 19:33:47 -0600

    Bandera de verificación de roles

--- util/rol-auth.js
+++ util/rol-auth.js
@@ -1,6 +1,22 @@
-module.exports = (rol) => (request, response, next) => {
+/*module.exports = (rol) => (request, response, next) => {
     // ir de rol en rol para verificar que si haya coincidencia   
     if (!(request.session.idRol == rol)) {
+        return response.status(403).redirect('./tops/');
+    }
+    next();
+}*/
+
+module.exports = (roles) => (request, response, next) => {
+
+    let r;
+    // ir de rol en rol para verificar que si haya coincidencia   
+    for (let rol of roles){
+        if (request.session.idRol == rol) {
+            r = true; // hay coincidencia con el rol
+        }
+    }
+    
+    if (!r){
         return response.status(403).redirect('./tops/');
     }
     next();
