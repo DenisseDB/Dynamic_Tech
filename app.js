713d0317ea11fbf5c4adde930530281f43a98007
Arisbeth Aguirre <a01274803@itesm.mx>
2022-03-29 17:30:08 -0600

    Filtración de preguntas por nivel con ajax

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.set('views', 'views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.use(cookieParser());
 app.use(session({
     secret: 'Hola Zebrands', 
--- routes/createfb.routes.js
+++ routes/createfb.routes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 const createfb = require('../controllers/createfb_controller');
 
-
 router.get('/generarFormato', isAuth, checkRoleAuth([1]), createfb.generarFormato);
+router.get('/buscarFormato/:valor', isAuth, checkRoleAuth([1]), createfb.buscarFormato);
 router.get('/', isAuth, checkRoleAuth([1]), createfb.root);
 
 
