12b5d632302f0b061fd6a1fde8a11f8d1f167633
A13xMtzL <A01276785@tec.mx>
2022-03-30 11:00:09 -0600

    Formato codigo

--- controllers/createfb_controller.js
+++ controllers/createfb_controller.js
@@ -1,56 +1,58 @@
 const formatoEvaluacion = require('../models/formatoEvaluacion');
 const Dimension = require('../models/dimension');
 const Pregunta = require('../models/pregunta');
-const { route } = require('../routes/createfb.routes');
+const {
+  route
+} = require('../routes/createfb.routes');
 
 exports.root = (request, response, next) => {
-    response.render('formatosEvaluacion');
+  response.render('formatosEvaluacion');
 };
 
 
 
-/*exports.generarFormato = (request, response, next) => {
-    Dimension.fetchAll()
-    .then(([rows1,fielData])=>{
-        Pregunta.findQuestions(request.body.inputGroupSelect02, request.body.inputGroupSelect01)
-        .then(([rows2,fielData])=>{
-            response.render('generarFormatos', {
-                dimensiones: rows1,
-                preguntas: rows2
+// exports.generarFormato = (request, response, next) => {
+//     Dimension.fetchAll()
+//       .then(([rows1, fielData]) => {
+//         Pregunta.findQuestions(request.body.inputGroupSelect02, request.body.inputGroupSelect01)
+//           .then(([rows2, fielData]) => {
+//             response.render('generarFormatos', {
+//               dimensiones: rows1,
+//               preguntas: rows2
 
-            });
-        }).catch((error)=>{
-            console.log(error);
-        });
-    }).catch((error)=>{
-        console.log(error);
-    });*/
+//             });
+//           }).catch((error) => {
+//             console.log(error);
+//           });
+//       }).catch((error) => {
+//         console.log(error);
+//       });
 
 
-exports.buscarFormato = (request, response, next) => {
-    console.log(request.params.nivel);
-    console.log(request.params.dim);
-    console.log('buscar');
-    Pregunta.findQuestions(request.params.nivel, request.params.dim)
+    exports.buscarFormato = (request, response, next) => {
+      console.log(request.params.nivel);
+      console.log(request.params.dim);
+      console.log('buscar');
+      Pregunta.findQuestions(request.params.nivel, request.params.dim)
         .then(([rows, fieldData]) => {
-            console.log(rows);
-            response.status(200).json(rows);
+          console.log(rows);
+          response.status(200).json(rows);
         })
         .catch(err => {
-            console.log(err);
+          console.log(err);
         });
-}
+    }
 
-exports.generarFormato = (request, response, next) => {
-    Dimension.fetchAll()
+    exports.generarFormato = (request, response, next) => {
+      Dimension.fetchAll()
         .then(([rows, fielData]) => {
-            response.render('generarFormatos', {
-                dimensiones: rows
-            });
+          response.render('generarFormatos', {
+            dimensiones: rows
+          });
         }).catch((error) => {
-            console.log(error);
+          console.log(error);
         });
 
-    /*response.render('generarFormatos', {
-    });*/
-};
+      /*response.render('generarFormatos', {
+      });*/
+    };
