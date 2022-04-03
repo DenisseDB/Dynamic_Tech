b18cb04efd2006c8672e123553f58e7171fb1d14
A13xMtzL <A01276785@tec.mx>
2022-04-03 15:09:47 -0500

    Revision consulta modelo Cuestionario

--- models/Cuestionario.js
+++ models/Cuestionario.js
@@ -6,7 +6,11 @@ module.exports = class Cuestionario {
 
    }
 
+   // static fetchAll() {
+   //    return db.execute('SELECT cuestionario.nombre FROM cuestionario NATURAL join preguntacuestionario NATURAL join pregunta INNER JOIN dimension on dimension.idDimension = pregunta.idDimension');
+   // }
    static fetchAll() {
-      return db.execute('SELECT cuestionario.nombre FROM cuestionario NATURAL join preguntacuestionario NATURAL join pregunta INNER JOIN dimension on dimension.idDimension = pregunta.idDimension');
+      return db.execute('SELECT DISTINCT(nombre_Dimension) FROM cuestionario NATURAL join preguntacuestionario NATURAL join pregunta INNER JOIN dimension on dimension.idDimension = pregunta.idDimension;');
    }
+
 };
--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
    host: 'localhost',
    user: 'DynamicTech',
-   database: 'bd_feedback2',
+   database: 'bd_feedback3',
    password: ''
 });
 
