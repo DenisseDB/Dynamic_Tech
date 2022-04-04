42eb2454e197992446d0ded8159d911877b6d821
A13xMtzL <A01276785@tec.mx>
2022-04-04 08:22:05 -0500

    Arreglar Controlador

--- models/Cuestionario.js
+++ models/Cuestionario.js
@@ -10,7 +10,7 @@ module.exports = class Cuestionario {
    //    return db.execute('SELECT cuestionario.nombre FROM cuestionario NATURAL join preguntacuestionario NATURAL join pregunta INNER JOIN dimension on dimension.idDimension = pregunta.idDimension');
    // }
    static fetchAll() {
-      return db.execute('SELECT DISTINCT(nombre_Dimension) FROM cuestionario NATURAL join preguntacuestionario NATURAL join pregunta INNER JOIN dimension on dimension.idDimension = pregunta.idDimension;');
+      return db.execute('SELECT * FROM cuestionario NATURAL join preguntacuestionario NATURAL join pregunta INNER JOIN dimension on dimension.idDimension = pregunta.idDimension;');
    }
 
 };
