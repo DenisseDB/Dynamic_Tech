d87e7ab33c2dae31fd605c6d08915328f147f954
JordanaBM <A01707434@itesm.mx>
2022-04-18 12:05:14 -0500

    Base de datos local

--- util/database.js
+++ util/database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
-/*const pool = mysql.createPool({
-    host: 'mysql-dynamictech.alwaysdata.net',
-    user: '263275',
-    database: 'dynamictech_bd_feedback1',
-    password: 'contraseña123.'
-});*/
+// const pool = mysql.createPool({
+//     host: 'mysql-dynamictech.alwaysdata.net',
+//     user: '263275',
+//     database: 'dynamictech_bd_feedback1',
+//     password: 'contraseña123.'
+// });
 
 const pool = mysql.createPool({
    host: 'localhost',
