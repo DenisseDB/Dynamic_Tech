1660b5f68be096690d543c5123e2840ae76c6242
Arisbeth Aguirre <a01274803@itesm.mx>
2022-04-19 14:03:30 -0500

    inicio stats

--- util/database.js
+++ util/database.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql2');
 
-// const pool = mysql.createPool({
-//     host: 'mysql-dynamictech.alwaysdata.net',
-//     user: '263275',
-//     database: 'dynamictech_bd_feedback1',
-//     password: 'contraseña123.'
-// });
-
 const pool = mysql.createPool({
+    host: 'mysql-dynamictech.alwaysdata.net',
+    user: '263275',
+    database: 'dynamictech_bd_feedback2',
+    password: 'contraseña123.'
+});
+
+/*const pool = mysql.createPool({
    host: 'localhost',
    user: 'DynamicTech',
    database: 'bd_feedback2',
    password: ''
-});
+});*/
 module.exports = pool.promise();
