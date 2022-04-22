fe17f7275dd5eb75c1142014e62d18fa2bce250b
A13xMtzL <A01276785@tec.mx>
2022-04-21 21:10:08 -0500

    No jala

--- util/database.js
+++ util/database.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2');
-// require('dotenv').config({path: './.env'});
+const config = require('../config');
 
 
 const pool = mysql.createPool({
-   connectionLimit : 10,
-   host: process.env.HOST,
-   user: process.env.USER,
-   password: process.env.PASSWORD,
-   database: process.env.DATABASE,
-   port:process.env.PORT
+   // connectionLimit : 10,
+   host: config.DB_HOST,
+   user: config.DB_USER,
+   database: config.DB_DATABASE,
+   password: config.DB_PASSWORD,
+   // port:process.env.PORT
 });
 
 // const pool = mysql.createPool({
