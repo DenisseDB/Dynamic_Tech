d40ab70316bc819fdc0e9204a2930852010d135a
A13xMtzL <A01276785@tec.mx>
2022-04-26 10:55:58 -0500

    Sin variables Entorno :'v

--- util/database.js
+++ util/database.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql2');
 const config = require('../config');
 
-const pool = mysql.createPool({
-   host: config.HOST,
-   user: config.USER,
-   database: config.DATABASE,
-   password: config.PASSWORD,
-});
-
 // const pool = mysql.createPool({
-//    host: 'localhost',
-//    user: 'DynamicTech',
-//    database: 'dynamictech_bd_feedback2',
-//    password: ''
+//    host: config.HOST,
+//    user: config.USER,
+//    database: config.DATABASE,
+//    password: config.PASSWORD,
 // });
+
+const pool = mysql.createPool({
+   host: 'mysql-dynamictech.alwaysdata.net',
+   user: '263275',
+   database: 'dynamictech_bd_feedback2',
+   password: 'contraseña123.'
+});
 
 module.exports = pool.promise();
