b0f6d7f41243c185f7438cd40c33e5272bda1b21
Arisbeth Aguirre <a01274803@itesm.mx>
2022-04-26 00:17:18 -0500

    Función ver detalle mis mentorados

--- util/database.js
+++ util/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 const config = require('../config');
 
 const pool = mysql.createPool({
-   host: 'mysql-dynamictech.alwaysdata.net',
-   user: '263275',
-   database: 'dynamictech_bd_feedback2',
-   password: 'contraseña123.'
+   host: config.HOST,
+   user: config.USER,
+   database: config.DATABASE,
+   password: config.PASSWORD,
 });
 
 // const pool = mysql.createPool({
