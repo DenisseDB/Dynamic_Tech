cc517cd47c3ae13af28ad96bb52f203cfb29547e
A13xMtzL <A01276785@tec.mx>
2022-04-26 10:59:13 -0500

    Agregar archivos faltantes

---
+++ config.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv').config();
+
+
+module.exports = {
+   HOST: process.env.HOST || '',
+   USER: process.env.USER || '',
+   DATABASE: process.env.DATABASE || '',
+   PASSWORD: process.env.PASSWORD || ''
+};
