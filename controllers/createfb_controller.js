5fff180fc88f1558d0808e1895cd10babbfd78bb
A13xMtzL <A01276785@tec.mx>
2022-04-27 14:31:24 -0500

    Cambio en controlador createfb

--- controllers/createfb_controller.js
+++ controllers/createfb_controller.js
@@ -5,207 +5,207 @@ const Cuestionario = require('../models/Cuestionario');
 
 exports.root = (request, response, next) => {
 
-    const info = request.session.info ? request.session.info : '';
-    request.session.info = '';
-    FormatoEvaluacion.fetchCuestionarios()
-        .then(([rows, fieldData]) => {
-            response.render('formatosEvaluacion', {
-                info: '',
-                nombreSesion: request.session.nombreSesion,
-                apellidoPSesion: request.session.apellidoPSesion,
-                foto: request.session.foto,
-                rolesA: request.session.privilegiosPermitidos,
-                cuestionarios: rows,
-            });
-        }).catch((error) => {
-            console.log(error);
-        });
+   const info = request.session.info ? request.session.info : '';
+   request.session.info = '';
+   FormatoEvaluacion.fetchCuestionarios()
+      .then(([rows, fieldData]) => {
+         response.render('formatosEvaluacion', {
+            info: '',
+            nombreSesion: request.session.nombreSesion,
+            apellidoPSesion: request.session.apellidoPSesion,
+            foto: request.session.foto,
+            rolesA: request.session.privilegiosPermitidos,
+            cuestionarios: rows,
+         });
+      }).catch((error) => {
+         console.log(error);
+      });
 };
 
 exports.editarCuestionario = (request, response, next) => {
 
-    response.render('EditarCuestionario', {
-        info: '',
-        nombreSesion: request.session.nombreSesion,
-        apellidoPSesion: request.session.apellidoPSesion,
-        foto: request.session.foto,
-        rolesA: request.session.privilegiosPermitidos,
-    });
+   response.render('EditarCuestionario', {
+      info: '',
+      nombreSesion: request.session.nombreSesion,
+      apellidoPSesion: request.session.apellidoPSesion,
+      foto: request.session.foto,
+      rolesA: request.session.privilegiosPermitidos,
+   });
 };
 
-exports.editarCuestionario_post = (request, response, next) => { };
+exports.editarCuestionario_post = (request, response, next) => {};
 
 exports.verCuestionario = (request, response, next) => {
 
-    response.render('verCuestionario', {
-        info: '',
-        nombreSesion: request.session.nombreSesion,
-        apellidoPSesion: request.session.apellidoPSesion,
-        foto: request.session.foto,
-        rolesA: request.session.privilegiosPermitidos,
-    });
+   response.render('verCuestionario', {
+      info: '',
+      nombreSesion: request.session.nombreSesion,
+      apellidoPSesion: request.session.apellidoPSesion,
+      foto: request.session.foto,
+      rolesA: request.session.privilegiosPermitidos,
+   });
 };
 
 exports.buscarFormato = (request, response, next) => {
-    //console.log(request.params.nivel);
-    //console.log(request.params.dim);
-    //console.log(request.body);
-    //console.log('buscar');
-    Pregunta.findQuestions(request.params.nivel, request.params.dim)
-        .then(([rows, fieldData]) => {
-            //console.log(rows);
-            response.status(200).json(rows);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+   //console.log(request.params.nivel);
+   //console.log(request.params.dim);
+   //console.log(request.body);
+   //console.log('buscar');
+   Pregunta.findQuestions(request.params.nivel, request.params.dim)
+      .then(([rows, fieldData]) => {
+         //console.log(rows);
+         response.status(200).json(rows);
+      })
+      .catch(err => {
+         console.log(err);
+      });
 };
 
 exports.generarFormato = (request, response, next) => {
 
-    //request.session.pr = 0;
+   //request.session.pr = 0;
 
-    Dimension.fetchAll()
-        .then(([rows, fieldData]) => {
-            response.render('generarFormatos', {
-                dimensiones: rows,
-                nombreSesion: request.session.nombreSesion,
-                apellidoPSesion: request.session.apellidoPSesion,
-                foto: request.session.foto,
-                nombreC: "",
-                dimC: "",
-                nivelC: "",
-                pregunta0_p: "",
-                pregunta1_p: "",
-                pregunta2_p: "",
-                rolesA: request.session.privilegiosPermitidos,
-                //pr: request.session.pr
-            });
-        }).catch((error) => {
-            console.log(error);
-        });
+   Dimension.fetchAll()
+      .then(([rows, fieldData]) => {
+         response.render('generarFormatos', {
+            dimensiones: rows,
+            nombreSesion: request.session.nombreSesion,
+            apellidoPSesion: request.session.apellidoPSesion,
+            foto: request.session.foto,
+            nombreC: "",
+            dimC: "",
+            nivelC: "",
+            pregunta0_p: "",
+            pregunta1_p: "",
+            pregunta2_p: "",
+            rolesA: request.session.privilegiosPermitidos,
+            //pr: request.session.pr
+         });
+      }).catch((error) => {
+         console.log(error);
+      });
 };
 
 exports.generarFormato_post = (request, response, next) => {
 
-    //console.log("Controlador:");
-    //console.log(request.body);
-    request.session.info = 'El cuestionario "' + request.body.nombreCuestionario + '" fue registrado con éxito';
-    const formatoEvaluacion = new FormatoEvaluacion(request.body.nombreCuestionario, request.body.inputDimension, request.body.inputNivel);
-    formatoEvaluacion.saveCuestionario()
-        .then(() => {
-            FormatoEvaluacion.fetchCuestionarios()
-                .then(([rows, fieldData]) => {
-                    response.render('formatosEvaluacion', {
-                        info: '',
-                        nombreSesion: request.session.nombreSesion,
-                        apellidoPSesion: request.session.apellidoPSesion,
-                        foto: request.session.foto,
-                        rolesA: request.session.privilegiosPermitidos,
-                        cuestionarios: rows,
-                    });
-                }).catch((error) => {
-                    console.log(error);
-                });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-    //response.render('generarFormatos', {
-    //});
+   //console.log("Controlador:");
+   //console.log(request.body);
+   request.session.info = 'El cuestionario "' + request.body.nombreCuestionario + '" fue registrado con éxito';
+   const formatoEvaluacion = new FormatoEvaluacion(request.body.nombreCuestionario, request.body.inputDimension, request.body.inputNivel);
+   formatoEvaluacion.saveCuestionario()
+      .then(() => {
+         FormatoEvaluacion.fetchCuestionarios()
+            .then(([rows, fieldData]) => {
+               response.render('formatosEvaluacion', {
+                  info: '',
+                  nombreSesion: request.session.nombreSesion,
+                  apellidoPSesion: request.session.apellidoPSesion,
+                  foto: request.session.foto,
+                  rolesA: request.session.privilegiosPermitidos,
+                  cuestionarios: rows,
+               });
+            }).catch((error) => {
+               console.log(error);
+            });
+      })
+      .catch(err => {
+         console.log(err);
+      });
+   //response.render('generarFormatos', {
+   //});
 };
 
 exports.crearPregunta1 = (request, response, next) => {
-    response.render('crearPregunta', {
-        nivelP: request.params.nivel,
-        dimP: request.params.dim,
-        nombreC: request.params.nombre,
-        pregunta0: "",
-        pregunta1: "",
-        pregunta2: "",
-        nombreSesion: request.session.nombreSesion,
-        apellidoPSesion: request.session.apellidoPSesion,
-        foto: request.session.foto,
-        rolesA: request.session.privilegiosPermitidos,
-    });
+   response.render('crearPregunta', {
+      nivelP: request.params.nivel,
+      dimP: request.params.dim,
+      nombreC: request.params.nombre,
+      pregunta0: "",
+      pregunta1: "",
+      pregunta2: "",
+      nombreSesion: request.session.nombreSesion,
+      apellidoPSesion: request.session.apellidoPSesion,
+      foto: request.session.foto,
+      rolesA: request.session.privilegiosPermitidos,
+   });
 };
 
 exports.crearPregunta2 = (request, response, next) => {
-    response.render('crearPregunta', {
-        nivelP: request.params.nivel,
-        dimP: request.params.dim,
-        nombreC: request.params.nombre,
-        pregunta0: request.params.pregunta0,
-        pregunta1: "",
-        pregunta2: "",
-        nombreSesion: request.session.nombreSesion,
-        apellidoPSesion: request.session.apellidoPSesion,
-        foto: request.session.foto,
-        rolesA: request.session.privilegiosPermitidos,
-    });
+   response.render('crearPregunta', {
+      nivelP: request.params.nivel,
+      dimP: request.params.dim,
+      nombreC: request.params.nombre,
+      pregunta0: request.params.pregunta0,
+      pregunta1: "",
+      pregunta2: "",
+      nombreSesion: request.session.nombreSesion,
+      apellidoPSesion: request.session.apellidoPSesion,
+      foto: request.session.foto,
+      rolesA: request.session.privilegiosPermitidos,
+   });
 };
 
 exports.crearPregunta3 = (request, response, next) => {
-    response.render('crearPregunta', {
-        nivelP: request.params.nivel,
-        dimP: request.params.dim,
-        nombreC: request.params.nombre,
-        pregunta0: request.params.pregunta0,
-        pregunta1: request.params.pregunta1,
-        pregunta2: "",
-        nombreSesion: request.session.nombreSesion,
-        apellidoPSesion: request.session.apellidoPSesion,
-        foto: request.session.foto,
-        rolesA: request.session.privilegiosPermitidos,
-    });
+   response.render('crearPregunta', {
+      nivelP: request.params.nivel,
+      dimP: request.params.dim,
+      nombreC: request.params.nombre,
+      pregunta0: request.params.pregunta0,
+      pregunta1: request.params.pregunta1,
+      pregunta2: "",
+      nombreSesion: request.session.nombreSesion,
+      apellidoPSesion: request.session.apellidoPSesion,
+      foto: request.session.foto,
+      rolesA: request.session.privilegiosPermitidos,
+   });
 };
 
 exports.crearPregunta4 = (request, response, next) => {
-    response.render('crearPregunta', {
-        nivelP: request.params.nivel,
-        dimP: request.params.dim,
-        nombreC: request.params.nombre,
-        pregunta0: request.params.pregunta0,
-        pregunta1: request.params.pregunta1,
-        pregunta2: request.params.pregunta2,
-        nombreSesion: request.session.nombreSesion,
-        apellidoPSesion: request.session.apellidoPSesion,
-        foto: request.session.foto,
-        rolesA: request.session.privilegiosPermitidos,
-    });
+   response.render('crearPregunta', {
+      nivelP: request.params.nivel,
+      dimP: request.params.dim,
+      nombreC: request.params.nombre,
+      pregunta0: request.params.pregunta0,
+      pregunta1: request.params.pregunta1,
+      pregunta2: request.params.pregunta2,
+      nombreSesion: request.session.nombreSesion,
+      apellidoPSesion: request.session.apellidoPSesion,
+      foto: request.session.foto,
+      rolesA: request.session.privilegiosPermitidos,
+   });
 };
 exports.crearPregunta_post = (request, response, next) => {
-    /*console.log('ab');
-    console.log(request.body.textoPregunta);
-    console.log(request.body.nivelPr);
-    console.log(request.body.dimPr);
-    console.log(request.body.inputTipo);*/
+   /*console.log('ab');
+   console.log(request.body.textoPregunta);
+   console.log(request.body.nivelPr);
+   console.log(request.body.dimPr);
+   console.log(request.body.inputTipo);*/
 
-    const pregunta = new Pregunta(request.body.textoPregunta, request.body.nivelPr, request.body.dimPr, request.body.inputTipo);
-    pregunta.savePregunta()
-        .then(() => {
-            Dimension.fetchAll()
-                .then(([rows, fieldData]) => {
-                    response.render('generarFormatos', {
-                        dimensiones: rows,
-                        nombreSesion: request.session.nombreSesion,
-                        apellidoPSesion: request.session.apellidoPSesion,
-                        nombreC: request.body.nombreC,
-                        dimC: request.body.dimPr,
-                        nivelC: request.body.nivelPr,
-                        pregunta0_p: request.body.pregunta0,
-                        pregunta1_p: request.body.pregunta1,
-                        pregunta2_p: request.body.pregunta2,
-                        foto: request.session.foto,
-                        rolesA: request.session.privilegiosPermitidos,
-                    });
-                }).catch((error) => {
-                    console.log(error);
-                });
-        })
-        .catch(err => {
-            console.log(err);
-        });
+   const pregunta = new Pregunta(request.body.textoPregunta, request.body.nivelPr, request.body.dimPr, request.body.inputTipo);
+   pregunta.savePregunta()
+      .then(() => {
+         Dimension.fetchAll()
+            .then(([rows, fieldData]) => {
+               response.render('generarFormatos', {
+                  dimensiones: rows,
+                  nombreSesion: request.session.nombreSesion,
+                  apellidoPSesion: request.session.apellidoPSesion,
+                  nombreC: request.body.nombreC,
+                  dimC: request.body.dimPr,
+                  nivelC: request.body.nivelPr,
+                  pregunta0_p: request.body.pregunta0,
+                  pregunta1_p: request.body.pregunta1,
+                  pregunta2_p: request.body.pregunta2,
+                  foto: request.session.foto,
+                  rolesA: request.session.privilegiosPermitidos,
+               });
+            }).catch((error) => {
+               console.log(error);
+            });
+      })
+      .catch(err => {
+         console.log(err);
+      });
 };
 
 
@@ -269,18 +269,16 @@ exports.buscarCuestionario = (request, response, next) => {
 exports.buscarPregunta = (request, response, next) => {
    console.log('buscar_Pregunta');
 
-   console.log("Buscap-> nivel: ",request.params.nivel);
-   console.log("Buscap-> dimension: ",request.params.dim);
-   console.log("Buscap-> cuestionario: ",request.params.cuest);
+   console.log("Buscap-> nivel: ", request.params.nivel);
+   console.log("Buscap-> dimension: ", request.params.dim);
+   console.log("Buscap-> cuestionario: ", request.params.cuest);
 
-   Cuestionario.findQuestions(request.params.nivel, request.params.dim,request.params.cuest)
+   Cuestionario.findQuestions(request.params.nivel, request.params.dim, request.params.cuest)
       .then(([rows, fieldData]) => {
          console.log(rows);
          response.status(200).json(rows);
       })
       .catch(err => {
          console.log(err);
-      }); 
+      });
 };
-
-
