e4340db6e2ca11311643e0b14f6e7d1d92eee07e
A13xMtzL <A01276785@tec.mx>
2022-04-27 22:17:07 -0500

    Pruebas para variables de entorno en instancia

--- config.js
+++ config.js
@@ -1,9 +1,20 @@
 const dotenv = require('dotenv').config();
 
+// * Variables de entorno con base de datos alwaysdata
+// module.exports = {
+//    HOST: process.env.HOST || '',
+//    USER: process.env.USER || '',
+//    DATABASE: process.env.DATABASE || '',
+//    PASSWORD: process.env.PASSWORD || ''
+// };
+// *
 
+//! Llamada a variable de entorno de la instancia
 module.exports = {
    HOST: process.env.HOST || '',
    USER: process.env.USER || '',
    DATABASE: process.env.DATABASE || '',
-   PASSWORD: process.env.PASSWORD || ''
+   PASSWORD: process.env.PASSWORD || '',
+   PORT: process.env.PORT || ''
 };
+// !
--- util/database.js
+++ util/database.js
@@ -1,28 +1,40 @@
 const mysql = require('mysql2');
 const config = require('../config');
 
+
+// * Variables de entorno con base de datos alwaysdata
+const pool = mysql.createPool({
+   host: config.HOST,
+   user: config.USER,
+   database: config.DATABASE,
+   password: config.PASSWORD,
+});
+// *
+
+// * pool para db local (borrar despues)
+
 // const pool = mysql.createPool({
-//    host: config.HOST,
-//    user: config.USER,
-//    database: config.DATABASE,
-//    password: config.PASSWORD,
+//    host: 'localhost',
+//    user: 'DynamicTech',
+//    database: 'dynamictech_bd_feedback2',
+//    password: ''
 // });
 
-const pool = mysql.createPool({
-   host: 'mysql-dynamictech.alwaysdata.net',
-   user: '263275',
-   database: 'dynamictech_bd_feedback2',
-   password: 'contraseña123.'
-});
+// *
 
+// ! pool creado para la base de datos de la instancia --> (No usar fuera de rama main)
 const DB_INSTANCIA =  mysql.createPool({
-   host: 'localhost',
-   user: 'admin',
-   database: 'dynamictech_bd_feedback2',
-   password: 'dynamic123.',
-   port: 3306
+   host: config.HOST,
+   user: config.USER,
+   database: config.DATABASE,
+   password: config.PASSWORD,
+   port: config.PORT,
 });
+// !
 
 
+module.exports = DB_INSTANCIA.promise(); // ! Se ejecuta este comando cuando queramos 
+                                         // ! solo ejecutar la bd de la instancia 
 
-module.exports = DB_INSTANCIA.promise();
+
+// module.exports = pool.promise();
