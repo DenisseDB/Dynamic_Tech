0cd91f28c1b1d3c6dfa249dc6524762430a01158
JordanaBM <A01707434@itesm.mx>
2022-05-03 09:48:40 -0500

    Posible correción formato evaluación

--- models/formatoEvaluacion.js
+++ models/formatoEvaluacion.js
@@ -3,41 +3,6 @@ const bcrypt = require('bcryptjs');
 
 module.exports = class formatoEvaluacion {
 
-<<<<<<< HEAD
-    //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
-    constructor(nuevo_nombre, nueva_dimension, nuevo_nivel, nueva_pregunta = []) {
-        this.nombre = nuevo_nombre;
-        this.nivel = nuevo_nivel;
-        this.dimension = nueva_dimension;
-        this.pregunta = nueva_pregunta;
-   }
-   
-
-    static fetchCuestionarios() {
-        //return db.execute('SELECT DISTINCT nombre, fecha, idDimension, nivelP FROM cuestionario C, preguntacuestionario PC, pregunta P WHERE C.idCuestionario = PC.idCuestionario AND PC.idPregunta = P.idPregunta; ');
-        return db.execute('SELECT DISTINCT nombre, fecha, idDimension, nivelP FROM cuestionario C, preguntacuestionario PC, pregunta P WHERE C.idCuestionario = PC.idCuestionario AND PC.idPregunta = P.idPregunta ORDER BY idDimension, fecha, nivelP ASC;');
-    }
-
-    // //Este método servirá para guardar de manera persistente el nuevo objeto. 
-    saveCuestionario() {
-        return db.execute('INSERT INTO cuestionario(nombre, fecha) VALUES(?, CURRENT_DATE()); ',
-            [this.nombre]);
-    }
-
-    savePreguntasCuestionario() {
-        let query = 'INSERT INTO preguntacuestionario VALUES';
-        let arr = [];
-
-        console.log(this.pregunta[0]);
-        console.log(this.pregunta[1]);
-        console.log(typeof this.pregunta[1] === 'undefined');
-
-
-        for (let i = 0; i < this.pregunta.length; i++) {
-            if (typeof this.pregunta[i] !== 'undefined') {
-                query += '(LAST_INSERT_ID(), ? , CURRENT_DATE())';
-                arr.push(this.pregunta[i]);
-=======
    // Constructor de la clase. Sirve para creaFr un nuevo objeto, y en él se definen las propiedades del modelo
    constructor(nuevo_nombre, nueva_dimension, nuevo_nivel) {
       this.nombre = nuevo_nombre;
@@ -48,7 +13,6 @@ module.exports = class formatoEvaluacion {
    static fetchCuestionarios() {
       return db.execute('SELECT DISTINCT nombre, idCuestionario, fecha, idDimension, nivelC FROM cuestionario_actual CA ORDER BY idDimension,nivelC ASC');
    }
->>>>>>> develop
 
    // Este método servirá para guardar de manera persistente el nuevo objeto. 
    saveCuestionario() {
